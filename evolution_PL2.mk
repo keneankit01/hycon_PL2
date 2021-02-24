82198e89532c4ea69c63454f6c8c82e2092de608
ANKIT KENE <47526753+keneankit01@users.noreply.github.com>
2021-02-24 18:32:27 +0530

    Update evolution_PL2.mk

--- evolution_PL2.mk
+++ evolution_PL2.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 #nherit from PL2 device
 $(call inherit-product, device/nokia/PL2/device.mk)
